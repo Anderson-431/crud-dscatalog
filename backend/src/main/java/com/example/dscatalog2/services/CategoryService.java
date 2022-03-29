74057092db17d23aca99fd37c1a5dbe81bc3ba27
Anderson Pinto <anderson.p20@gmail.com>
2022-03-29 20:10:32 -0300

    Database seeding

--- backend/src/main/java/com/example/dscatalog2/services/CategoryService.java
+++ backend/src/main/java/com/example/dscatalog2/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.dscatalog2.entities.Category;
 import com.example.dscatalog2.repositories.CategoryRepository;
@@ -16,8 +17,14 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
 
 }
+
+/* @Transactional(readOnly = true): O parâmetro “readOnly” na anotação @Transactional 
+  especifica que nenhuma operação de DML poderá ser executada (Insert, Delete ou Update),
+  ou seja, apenas consultas.
+*/
