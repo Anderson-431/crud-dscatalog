9dce29695fda83e5b3582623d8ec3690f91de0e3
Anderson Pinto <anderson.p20@gmail.com>
2022-04-05 20:35:18 -0300

    Update category

--- backend/src/main/java/com/example/dscatalog2/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/example/dscatalog2/resources/exceptions/ResourceExceptionHandler.java
@@ -9,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.example.dscatalog2.services.exceptions.EntityNotFoundException;
+import com.example.dscatalog2.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest requeste){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest requeste){
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(HttpStatus.NOT_FOUND.value());
--- backend/src/main/java/com/example/dscatalog2/services/CategoryService.java
+++ backend/src/main/java/com/example/dscatalog2/services/CategoryService.java
@@ -4,17 +4,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.dscatalog2.dto.CategoryDTO;
 import com.example.dscatalog2.entities.Category;
 import com.example.dscatalog2.repositories.CategoryRepository;
-import com.example.dscatalog2.services.exceptions.EntityNotFoundException;
+import com.example.dscatalog2.services.exceptions.ResourceNotFoundException;
 
 			//Camada de Serviço
 
@@ -33,7 +32,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
 
@@ -44,8 +43,19 @@ public class CategoryService {
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	}
-	
-	
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+		Category entity = repository.getOne(id);
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+	}	
 }
 
 /* @Transactional(readOnly = true): O parâmetro “readOnly” na anotação @Transactional 
